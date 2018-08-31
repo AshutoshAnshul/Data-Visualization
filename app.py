b33beb67cf01cd9bf6db0cc9937dc38adb03d627
AshutoshAnshul <32036815+AshutoshAnshul@users.noreply.github.com>
2018-09-01 02:36:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -13,12 +13,20 @@ def input():
 
 @app.route("/submit",methods=["POST"])
 def show():
-	year=list(map(float,request.form["Year"].split(",")))
-	pop=list(map(float,request.form["Population"].split(",")))
-
-	img=BytesIO()
-	plt.plot(year,pop)
-	plt.xlabel("Year")
-	plt.ylabel("Population in billions")
-	plt.savefig(img)
-	return render_template("submit.html",img=base64.b64encode(img.getvalue()).decode())
+    year=list(map(float,request.form["Year"].split(",")))
+    pop=list(map(float,request.form["Population"].split(",")))
+    img=BytesIO() 
+    if request.form["graphs"]=="line" :
+        plt.figure()
+        plt.plot(year,pop)
+    elif request.form["graphs"]=="bar" :
+        plt.figure()
+        plt.bar(year,pop,color='g',width=5.0)
+    elif request.form["graphs"]=="scatter" :
+        plt.figure()
+        plt.scatter(year,pop,color='b')
+    plt.xlabel("Year")
+    plt.ylabel("Population in billions")
+    plt.title("Year vs Population")
+    plt.savefig(img)
+    return render_template("submit.html",img=base64.b64encode(img.getvalue()).decode())
